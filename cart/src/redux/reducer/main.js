cd00c83c7c8dae724fab64ba3b706b549ecbe2cb
MilanS3333 <110888010+MilanS3333@users.noreply.github.com>
2023-05-17 19:18:05 +0530

    d

--- cart/src/redux/reducer/main.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { cartreducer } from "./reducer";
-
-// compbine all reducers in one variable
-const rootreducer = combineReducers({
-    cartreducer
-});
-
-export default rootreducer;
--- cart/src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import rootreducer from "./redux/reducer/main";
-
-// get data from reducer and send specific file
-const store = createStore(
-    rootreducer
-)
-
-export default store;
